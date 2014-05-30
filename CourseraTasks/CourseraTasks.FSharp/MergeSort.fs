5e26d2d59345fd0022b7583d45284fa678c8e849
Andrey Korzenikov <andr_korz@mail.ru>
2014-05-30 15:49:13 +0400

    fixed split

--- CourseraTasks/CourseraTasks.FSharp/MergeSort.fs
+++ CourseraTasks/CourseraTasks.FSharp/MergeSort.fs
@@ -2,24 +2,33 @@
 
 module MergeSort =
 
+    let split list = 
+       let rec splitRec (list:List<_>) count acc = 
+            if count = 0 then
+                (List.rev acc, list)
+            else
+                splitRec list.Tail (count - 1) (list.Head :: acc)
+   
+       splitRec list (list.Length/2) []
+
     let merge left right  =
-        let rec tailRecursiveMerge left right acc = 
-            match left, right with
-            | [], list | list, [] -> List.rev acc @  list
-            | lh::lt, rh::rt -> 
-                if lh < rh then tailRecursiveMerge lt right (lh :: acc)
-                else tailRecursiveMerge left rt (rh :: acc)
+      let rec tailRecursiveMerge left right acc = 
+          match left, right with
+          | [], list | list, [] -> List.rev acc @  list
+          | lh::lt, rh::rt -> 
+             if lh < rh then tailRecursiveMerge lt right (lh :: acc)
+             else tailRecursiveMerge left rt (rh :: acc)
   
-        tailRecursiveMerge left right []
+      tailRecursiveMerge left right []
+
 
     let mergeSort (list:seq<_>) =
-        let rec mergeSortList (list:List<_>) =
-            let length = list.Length
-            if (length <= 1) then
-                list
-            else
-                let left  = list |> Seq.take (length / 2) |> List.ofSeq |> mergeSortList
-                let right = list |> Seq.skip (length / 2) |> List.ofSeq |> mergeSortList
-                merge left right
-     
-        list |> List.ofSeq |> mergeSortList |> Seq.ofList
+        let rec mergeSortList list =
+            match list with
+            | [] -> [] | head::[] -> list
+            | _ ->
+                let parts = split list
+                match parts with
+                    | (left, right) -> merge (mergeSortList left) (mergeSortList right)
+
+        list |> List.ofSeq |> mergeSortList
