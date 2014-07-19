0d728f37f51a50d28b79fa7d4d251b9b9ce63a73
korzenikov <andr_korz@mail.ru>
2014-07-19 16:35:47 +0400

    Changed placement of InputFiles

---
+++ CourseraTasks/CourseraTasks/ClusteringBigTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseraTasks
+{
+    public class ClusteringBigTask  : ITask
+    {
+        public void Run()
+        {
+            using (var reader = new StreamReader("InputFiles//clustering_big.txt"))
+            using (var writer = new StreamWriter("output.txt"))
+            {
+                reader.ReadLine();
+                while (true)
+                {
+                    string row = reader.ReadLine();
+                    if (row == null)
+                    {
+                        break;
+                    }
+
+                    var parts = row.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    var bits = parts.Select(x => int.Parse(x, CultureInfo.InvariantCulture)).ToArray();
+                }
+            }
+        }
+    }
+}
