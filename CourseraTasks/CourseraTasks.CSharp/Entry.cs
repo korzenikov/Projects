9c9734e6acde85e0a442335c0e8eb3514fb5fd45
Andrey Korzenikov <andr_korz@mail.ru>
2014-12-05 11:53:03 +0300

    Added custom dictionary implementation

---
+++ CourseraTasks/CourseraTasks.CSharp/Entry.cs
@@ -0,0 +1,16 @@
+﻿namespace CourseraTasks.CSharp
+{
+    public class Entry<TKey, TValue>
+    {
+        public Entry(TKey key)
+        {
+            Key = key;
+        }
+
+        public TKey Key { get; private set; }
+
+        public TValue Value { get; set; }
+
+        public Entry<TKey, TValue> Next { get; set; }
+    }
+}
