50029358f6976e4ecba61f6d5f684c9215e067d1
korzenikova <Andrei.Korzenikov@aqr.com>
2015-10-16 18:08:23 -0400

    Moved WaterMeasurement to GitHub

---
+++ WaterMeasurement/FuncWaterMeasurement/Program.fs
@@ -0,0 +1,29 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+let rec measureWater levels =
+    let rec measureWaterRec lowerBorder upperBorder total levels =
+        match lowerBorder > upperBorder with
+        | true -> levels |> List.rev |> measureWaterRec upperBorder lowerBorder total 
+        | false ->
+            match levels with
+            | [] -> total
+            | head::tail -> let diff = lowerBorder - head
+                            match diff > 0 with
+                            | true ->  measureWaterRec lowerBorder upperBorder (total + diff) tail
+                            | false->  measureWaterRec head upperBorder total tail
+    match levels with
+    | [] -> 0
+    | [_] -> 0
+    | head::tail -> measureWaterRec head (Seq.last tail) 0 levels
+
+[<EntryPoint>]
+let main argv = 
+    let buildingHeights1 = [ 5; 4; 3; 2; 1; 2; 3; 4]
+    let buildingHeights2 = [ 2; 1; 4; 3; 4; 1; 2 ]
+    let buildingHeights3 = [ 2; 5; 1; 2; 3; 4; 7; 7; 6 ]
+    let buildingHeights4 = [ 1; 3; 5; 6; 4; 2 ]
+    printfn "%A" (measureWater buildingHeights1)
+    printfn "%A" (measureWater buildingHeights2)
+    printfn "%A" (measureWater buildingHeights3)
+    printfn "%A" (measureWater buildingHeights4)
+    0 // return an integer exit code
---
+++ WaterMeasurement/WaterMeasurement/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace WaterMeasurement
+{
+    class Program
+    {
+        public static int MeasureWater(int[] buildingHeights)
+        {
+            var water = 0;
+
+            var length = buildingHeights.Length;
+            var leftIndex = 0;
+            var rightIndex = length - 1;
+            var leftMaximum = buildingHeights[leftIndex];
+            var rightMaximum = buildingHeights[rightIndex];
+            while (leftIndex <= rightIndex)
+            {
+                // Move from left to right
+                if (leftMaximum <= rightMaximum)
+                {
+                    var height = buildingHeights[leftIndex];
+                    if (height > leftMaximum)
+                        leftMaximum = height;
+                    else
+                        water += leftMaximum - height;
+                    leftIndex++;
+                }
+                else
+                {
+                    var height = buildingHeights[rightIndex];
+                    if (height > rightMaximum)
+                        rightMaximum = height;
+                    else
+                    {
+                        water += rightMaximum - height;
+                    }
+                    rightIndex--;
+                }
+            }
+
+            return water;
+        }
+
+        static void Main(string[] args)
+        {
+            var buildingHeights1 = new[] { 5, 4, 3, 2, 1, 2, 3, 4};
+            var buildingHeights2 = new[] { 2, 1, 4, 3, 4, 1, 2 };
+            var buildingHeights3 = new[] { 2, 5, 1, 2, 3, 4, 7, 7, 6 };
+            var tests = new[] { buildingHeights1, buildingHeights2, buildingHeights3 };
+            for (int i = 0; i < tests.Length; i++)
+            {
+                Console.WriteLine("Test {0}: {1}", i + 1, MeasureWater(tests[i]));
+            }
+        }
+    }
+}
