ec8f0c10f8fcfec3b325c3bfb7433cc9bf6a63ae
korzenikova <Andrei.Korzenikov@aqr.com>
2015-10-30 16:57:19 -0400

    new test cases

--- WaterMeasurement/WaterMeasurement/Program.cs
+++ WaterMeasurement/WaterMeasurement/Program.cs
@@ -41,15 +41,37 @@ namespace WaterMeasurement
             return water;
         }
 
+        public static int MeasureWater2(int[] data)
+        {
+            int a = 0, z, sum = 0;
+            while (a < data.Length - 1)
+            {
+                for (z = a + 1; z < data.Length - 1 && data[z] < data[a]; z++);
+                int max = Math.Min(data[a], data[z]);
+                for (a = a + 1; a < z; a++) sum += Math.Max(0, max - data[a]);
+            }
+
+
+            return sum;
+        }
+
         static void Main(string[] args)
         {
             var buildingHeights1 = new[] { 5, 4, 3, 2, 1, 2, 3, 4};
             var buildingHeights2 = new[] { 2, 1, 4, 3, 4, 1, 2 };
             var buildingHeights3 = new[] { 2, 5, 1, 2, 3, 4, 7, 7, 6 };
-            var tests = new[] { buildingHeights1, buildingHeights2, buildingHeights3 };
+            var buildingHeights4 = new[] { 5, 7, 5, 4, 9, 8, 6, 7, 4, 9, 6, 4 };
+            var buildingHeights5 = new[] { 1, 3, 5, 6, 4, 2 };
+            var buildingHeights6 = new[] { 3, 1, 2 };
+            var buildingHeights7 = new[] { 9, 8, 6, 7, 4 };
+
+
+            var tests = new[] { buildingHeights1, buildingHeights2, buildingHeights3, buildingHeights4, buildingHeights5, buildingHeights6, buildingHeights7 };
             for (int i = 0; i < tests.Length; i++)
             {
-                Console.WriteLine("Test {0}: {1}", i + 1, MeasureWater(tests[i]));
+                Console.WriteLine("Test C#  {0}: {1}", i + 1, MeasureWater(tests[i]));
+                Console.WriteLine("Test C#2 {0}: {1}", i + 1, MeasureWater2(tests[i]));
+                Console.WriteLine("Test F#  {0}: {1}", i + 1, WMFLIb.WaterMeasurement.measureWater(tests[i]));
             }
         }
     }
