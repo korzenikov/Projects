33f4b964cf3de2e942376acacd8824190639c3cc
Andrei Korzenikov <korzenikoval@gmail.com>
2015-12-09 20:56:34 -0500

    Fixed bug

--- TryFSharp/TryFSharp/Layout.fs
+++ TryFSharp/TryFSharp/Layout.fs
@@ -1,35 +1,39 @@
 ﻿open System
 
-let maxSquareEdge (a: double) (b: double) N  =
+let maxSquareEdge (a: float) (b: float) N  =
     let w, h =  if a > b then a, b else b, a
     let rec maxSquareEdgeRec r = 
-        let c = Math.Ceiling(double N / double r) |> int
+        let c = Math.Ceiling(float N / float r) |> int
         match r <= c with
         | true -> 
-            let cur = Math.Min( w / double c, h / double r)
-            printfn "%f %d %d" cur r c
+            let cur = Math.Min( w / float c, h / float r)
+            //printfn "%f %d %d" cur r c
             Math.Max (cur, maxSquareEdgeRec (r + 1)) 
         | false -> 0.0
     maxSquareEdgeRec 1
 
-let maxSquareEdgeExt (a: double) (b: double) N  =
-    let r = b / a * double N |> Math.Sqrt|> Math.Round |> int
-    let c = double N / double r |> Math.Ceiling |> int
-    Math.Min( a / double c, b / double r)
-    
+let maxSquareEdgeExt (a: float) (b: float) N  =
+    let w, h =  if a > b then a, b else b, a
+    let rec maxSquareEdgeRec r = 
+        let c = Math.Ceiling(float N / float r) |> int
+        let cur = Math.Min( w / float c, h / float r)
+        //printfn "%f %d %d" cur r c
+        cur
+    let r = Math.Sqrt(h / w * float N) |> int
+    Math.Max(maxSquareEdgeRec r, maxSquareEdgeRec (r + 1))
 
-printfn "%f" (maxSquareEdge 3.0 2.0 7)
-printfn "%A" (maxSquareEdgeExt 3.0 2.0 7)
+let parameters =
+  seq { for a in 1..100 do
+            for b in 1..100 do
+                for N in 1..1000 do
+                    yield (a, b, N) }
 
-printfn "%f" (maxSquareEdge 8.0 4.0 15)
-printfn "%A" (maxSquareEdgeExt 8.0 4.0 15)
+parameters 
+    |> Seq.map (fun (a, b, N) -> a, b, N, maxSquareEdgeExt (float a) (float b) N, maxSquareEdge (float a) (float b) N)
+    |> Seq.filter (fun (a, b, N, res1, res2) -> Math.Abs(res1 - res2) > 0.0)
+    |> Seq.iter (fun (a, b, N, res1, res2) -> printfn "%d %d %d: %f %f" a b N res1 res2 ) 
 
 
-printfn "%f" (maxSquareEdge 3.0 4.0 11)
-printfn "%A" (maxSquareEdgeExt 3.0 4.0 11)
-printfn "%A" (maxSquareEdgeExt 4.0 3.0 11)
 
 
-printfn "%f" (maxSquareEdge 2.0 6.0 11)
-printfn "%A" (maxSquareEdgeExt 2.0 6.0 11)
-printfn "%A" (maxSquareEdgeExt 6.0 2.0 11)
+
