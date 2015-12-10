e53b5781b40eb48086b7a736d102aa0cd8d5cd5c
korzenikova <Andrei.Korzenikov@aqr.com>
2015-12-09 19:10:23 -0500

    Found a simpler solution to layouting problem

--- TryFSharp/TryFSharp/Layout.fs
+++ TryFSharp/TryFSharp/Layout.fs
@@ -2,13 +2,34 @@
 
 let maxSquareEdge (a: double) (b: double) N  =
     let w, h =  if a > b then a, b else b, a
-    let rec maxSquareEdgeRec r c = 
+    let rec maxSquareEdgeRec r = 
+        let c = Math.Ceiling(double N / double r) |> int
         match r <= c with
         | true -> 
             let cur = Math.Min( w / double c, h / double r)
             printfn "%f %d %d" cur r c
-            Math.Max (cur, maxSquareEdgeRec (r + 1) (Math.Ceiling(double N / double (r + 1)) |> int))
+            Math.Max (cur, maxSquareEdgeRec (r + 1)) 
         | false -> 0.0
-    maxSquareEdgeRec 1 N
+    maxSquareEdgeRec 1
+
+let maxSquareEdgeExt (a: double) (b: double) N  =
+    let r = b / a * double N |> Math.Sqrt|> Math.Round |> int
+    let c = double N / double r |> Math.Ceiling |> int
+    Math.Min( a / double c, b / double r)
+    
+
+printfn "%f" (maxSquareEdge 3.0 2.0 7)
+printfn "%A" (maxSquareEdgeExt 3.0 2.0 7)
 
 printfn "%f" (maxSquareEdge 8.0 4.0 15)
+printfn "%A" (maxSquareEdgeExt 8.0 4.0 15)
+
+
+printfn "%f" (maxSquareEdge 3.0 4.0 11)
+printfn "%A" (maxSquareEdgeExt 3.0 4.0 11)
+printfn "%A" (maxSquareEdgeExt 4.0 3.0 11)
+
+
+printfn "%f" (maxSquareEdge 2.0 6.0 11)
+printfn "%A" (maxSquareEdgeExt 2.0 6.0 11)
+printfn "%A" (maxSquareEdgeExt 6.0 2.0 11)
